74fdda5f561e6681e0e82bb84956996a02c9a5ee
Jayanth-Rookie <jayanthoffical18@gmail.com>
2025-03-16 12:30:07 +0530

    Add datatypes.js with example code

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("jay");
+
+let name ="jay";
+let age = 25;
+let isApproved = true;
+
+// bigint
+//bool
+//string
+//null
+//undefined
+//object
+
+console.log(typeof name);
