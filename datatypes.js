3381f67b448bde45b5501aa3c926a3989fe7176c
Jayanth-Rookie <jayanthoffical18@gmail.com>
2025-03-16 12:53:33 +0530

    Update datatype operation

--- datatypes.js
+++ datatypes.js
@@ -1,23 +1,40 @@
-"use strict";
+// "use strict";
 
-console.log("jay");
+// console.log("jay");
 
-let name ="jay";
-let age = 25;
-let isApproved = true;
+// let name ="jay";
+// let age = 25;
+// let isApproved = true;
 
-// bigint
-//bool
-//string
-//null
-//undefined
-//object
+// // bigint
+// //bool
+// //string
+// //null
+// //undefined
+// //object
 
-console.log(typeof name);
+// console.log(typeof name);
 
-let score = "100";
+// let score = "100";
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber); // conversion string to number 
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // conversion string to number 
+
+// operations 
+
+let value = 10;
+let value2 = 20;
+
+// console.log(2+2);
+// console.log(value + value2);
+
+let str1= "hey";
+let str2 = "there";
+let str3 = str1 + " " + str2;
+// console.log(str3);
+
+let count = 100;
+count ++;
+console.log(count);
