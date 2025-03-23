ef60fe2b429a7b4f28821dad8052faff4cc69698
Jayanth-Rookie <jayanthoffical18@gmail.com>
2025-03-23 16:20:15 +0530

    Your commit message

---
+++ control.js
@@ -0,0 +1,74 @@
+const join = 40
+
+if(join === 30){
+    // console.log("join is less than 30")
+
+}
+
+// console.log("join is greater than 30")
+
+const score = 200
+
+if(score >100)
+{
+    const powe = "air"
+    // console.log(`user power ${powe}`)
+}
+
+// const mont = 1
+
+// switch(mont) {
+//     case 1:
+//         console.log("jan")
+//         break
+//     case 2:
+//         console.log("feb")
+//         break
+//     case 3:
+//         console.log("mar")
+//         break
+//     case 4:
+//         console.log("apr")
+//         break
+//     case 5:
+//         console.log("may")
+//         break
+//     case 6:
+//         console.log("jun")
+//         break
+//     case 7:
+//         console.log("jul")
+//         break
+//     case 8:
+//         console.log("aug")
+//         break
+//     case 9:
+//         console.log("sep")
+//         break
+//     case 10:
+//         console.log("oct")
+// }
+
+
+const use = "john"
+
+// falsy value
+
+false ,0,-0,0n,NaN,"",null,undefined
+
+// truty values 
+true,{},[],"0"," ",function(){}
+
+const key = "john"
+
+if(Object.keys(key).length > 0){
+    console.log("you have a key")
+}
+
+// Nullish coalesacling operator (??)
+
+let val1;
+
+val1 = 4 ?? 50
+
+console.log(val1)
